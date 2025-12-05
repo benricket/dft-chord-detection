948cabc6eff48f8dffb9ad46e509ac04347e000b
Ben Ricket <ben.ricket@gmail.com>
2025-12-05 14:49:12 -0500

    Add axis labels

--- process_audio.py
+++ process_audio.py
@@ -12,14 +12,17 @@ def main(path="chords.mp3"):
     print(f"Audio data shape: {data.shape}")
 
     processed = librosa.stft(data,n_fft=1024)
+    freq = librosa.fft_frequencies(sr=sample_rate,n_fft=1024)
     print(f"Processed: {processed}")
     print(f"Processed shape: {processed.shape}")
 
-    real = np.real(processed)
+    real = np.abs(processed)
     print(f"shape: {real.shape}")
     print(f"Min real: {np.min(real)}, max real: {np.max(real)}, std dev: {np.std(real)}")
+    print(f"Freqs: {freq}")
     fig,ax = plt.subplots()
-    im = ax.imshow(real,norm=matplotlib.colors.LogNorm(),cmap="plasma")
+    print(len(data)/sample_rate)
+    im = ax.imshow(real,norm=matplotlib.colors.LogNorm(),cmap="plasma",extent=[0,len(data)/sample_rate,freq[-1],0.0],aspect=0.001)
     plt.colorbar(im)
     plt.show()
 
