a19dc72a4fbc68fcfae7e035882076438e7e34ae
Ben Ricket <ben.ricket@gmail.com>
2025-12-18 18:21:40 -0500

    Clean up code and output notes at what times they occur

--- process_audio.py
+++ process_audio.py
@@ -1,22 +1,29 @@
 import sys
-import os
-import time
 import numpy as np
 import scipy
 import librosa
 import matplotlib.pyplot as plt
 import matplotlib.colors
 
-SAMPLE_INDEX_OFFSET = 0 # For every peak difference, we assume our note is prominent this many samples later
-ARPEGGIO_THRESH = 0.25 # seconds 
-ARPEGGIO_THRESH_SAMPLES = 20
+SAMPLE_INDEX_OFFSET = 0 # For every peak diff, assume our note is prominent this many samples later
+DO_LOGGING = False
+ARPEGGIO_THRESH = 0.25 # seconds
+ARPEGGIO_THRESH_STEPS = 20
 
-def main(path="chord_test.mp3"):
+def main(path: str="chord_test.mp3"):
+    """
+    Runs the chord detection algorithm, which is contained within one function
+    for simplicity's sake
+
+    Args:
+        path (str): File path to the audio file to process
+    """
     data,sample_rate = librosa.load(path)
     print(data)
     print(f"Audio data shape: {data.shape}")
 
-    processed = librosa.stft(data,n_fft=4096)
+
+    processed = librosa.stft(data,n_fft=4096,win_length=4096,hop_length=512)
     freq = librosa.fft_frequencies(sr=sample_rate,n_fft=4096)
     print(f"Processed: {processed}")
     print(f"Processed shape: {processed.shape}")
@@ -43,17 +50,14 @@ def main(path="chord_test.mp3"):
     notes = []
     for i in range(9):
         notes.append({k / (2**(4-i)): v for k,v in notes_4.items()})
-    
-    #print (notes)
 
     low_hz = 10.0
     high_hz = 1000.0
     low_idx = np.where(freq >= low_hz)[0][0]
     high_idx = np.where(freq >= high_hz)[0][0]
-    print(f"low idx, hi idx: {low_idx}, {high_idx}")
 
-    thresh = np.quantile(real.flat,0.90)
-    real_masked = (real > thresh) * real
+    if DO_LOGGING:
+        print(f"low idx, hi idx: {low_idx}, {high_idx}")
 
     # Filtered amplitudes and frequencies
     real = real[low_idx:high_idx,:]
@@ -70,37 +74,55 @@ def main(path="chord_test.mp3"):
 
     chord_indices = peaks[0] + SAMPLE_INDEX_OFFSET
     chord_indices = [idx for idx in chord_indices if idx < real.shape[1] and real_diff_sum[idx] > diff_thresh]
+    chord_indices_filtered = []
+    chord_times = []
+    chords = {}
 
-    for i in range(len(chord_indices)):
-        chord_index = chord_indices[i]
-        if chord_index < len(chord_indices) - 1: # if we're not the last note
-            if chord_indices[i+1] - chord_index <= ARPEGGIO_THRESH_SAMPLES: # notes are in an arpeggio
+    print([(i,val) for i,val in enumerate(chord_indices)])
+    for i,val in enumerate(chord_indices):
+        if i < len(chord_indices) - 1: # if we're not the last note
+            if chord_indices[i+1] - val <= ARPEGGIO_THRESH_STEPS: # notes are in an arpeggio
                 continue
 
         # We've detected a chord at this sample index
-        chord_freqs = real[:,chord_index]
+        progress_seconds = (val / len(real_diff_sum)) * runtime_seconds
+        print(f"seconds: {progress_seconds}")
+        seconds_rounded = round(progress_seconds * 1000) / 1000
+        chord_indices_filtered.append(val)
+        chord_times.append(progress_seconds)
+        chord_freqs = real[:,val]
+
+        # Identify most prominent frequencies
         freq_peaks_idx = scipy.signal.find_peaks(chord_freqs,prominence=2)
         freq_peaks = freq[freq_peaks_idx[0]]
-        #print(f"Frequencies of notes: {freq_peaks}")
 
-        progress_seconds = (chord_index / len(real_diff_sum)) * runtime_seconds
-        print(f"{progress_seconds} seconds: {librosa.hz_to_note(freq_peaks)}")
+        # Get note names for identified frequencies
+        notes = librosa.hz_to_note(freq_peaks)
+        chords[seconds_rounded] = notes
 
-    #print(f"Peaks: {peaks}")
+    if DO_LOGGING:
+        print(f"Peaks: {peaks}")
+        print(f"Shape of diff: {real_diff.shape}")
+        print(f"Real diff sum: {real_diff_sum}")
+        print(f"shape: {real.shape}")
+        print(f"Min real: {np.min(real)}, max real: {np.max(real)}, std dev: {np.std(real)}")
+        print(f"Freqs: {freq}")
+    print(f"{progress_seconds} seconds: {librosa.hz_to_note(freq_peaks)}")
 
-    #print(f"Shape of diff: {real_diff.shape}")
-    #print(f"Real diff sum: {real_diff_sum}")
+    for s,n in chords.items():
+        print(f"{s} seconds: {n}")
 
-    #print(f"shape: {real.shape}")
-    #print(f"Min real: {np.min(real)}, max real: {np.max(real)}, std dev: {np.std(real)}")
-    #print(f"Freqs: {freq}")
-    fig,ax = plt.subplots(3,1)
-    #print(len(data)/sample_rate)
-    im0 = ax[0].imshow(real,norm=matplotlib.colors.LogNorm(),cmap="plasma",extent=[0,len(data)/sample_rate,freq[-1],0.0],aspect=0.001)
-    im_mask = ax[1].imshow(real_diff,norm=matplotlib.colors.LogNorm(),cmap="plasma",extent=[0,len(data)/sample_rate,freq[-1],0.0],aspect=0.001)
-    diffs = ax[2].plot(real_diff_sum)
-    ax[2].scatter(chord_indices,real_diff_sum[chord_indices],s=10,c='red')
-    plt.colorbar(im0)
+    _,ax = plt.subplots(2,1)
+    im0 = ax[0].imshow(real,norm=matplotlib.colors.LogNorm(),cmap="plasma",extent=[0,len(data)/sample_rate,freq[-1],0.0],aspect=0.002)
+    ax[0].set_xlabel("Time (seconds)")
+    ax[0].set_ylabel("Frequency component (Hz)")
+    ax[0].set_title("Spectrogram of audio track, displaying log frequency magnitudes for each time window")
+    ax[1].plot(real_diff_sum)
+    ax[1].set_xlabel("Time (seconds)")
+    ax[1].set_ylabel("Sum of absolute frequency magnitude differences (unitless)")
+    ax[1].set_title("Sums across all frequencies of absolute differences in frequency magnitude between time windows")
+    ax[1].scatter(chord_indices_filtered,real_diff_sum[chord_indices_filtered],s=10,c='red')
+
     plt.show()
 
 if __name__ == "__main__":
